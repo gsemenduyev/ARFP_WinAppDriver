b52e7dbc51b83cd42312b2f358f59710273d2c34
gsemenduyev <gsemenduyev@freewheel.com>
2024-08-17 13:24:27 -0500

    Added run run

---
+++ E2E/ARFP-Stratasphere/sbms-stratasphere-html-report.js
@@ -0,0 +1,58 @@
+const report = require('multiple-cucumber-html-reporter');
+const dayjs = require('dayjs');
+const fs = require('fs');
+const data = fs.readFileSync('cypress/reports/run-info/report-metadata.json', { encoding: 'utf8', flag: 'r' });
+const runInfo = JSON.parse(data);
+
+const osName = () => {
+    switch (runInfo['osName']) {
+        case 'darwin':
+            return 'osx';
+        case 'win32':
+            return 'windows';
+        case 'ubuntu':
+            return 'ubuntu';
+        case 'linux':
+            return 'linux';
+        default:
+            console.log('Undefined browser');
+    }
+};
+
+report.generate({
+    jsonDir: 'cypress/reports/sbms-arfp-stratasphere-report',
+    reportPath: 'cypress/reports/multi-html-report',
+    metadata: {
+        browser: {
+            name: runInfo['browserName'],
+            version: runInfo['browserVersion'],
+        },
+        device: 'Local Test Machine',
+        platform: {
+            name: osName(),
+            version: runInfo['osVersion'],
+        },
+    },
+    customData: {
+        title: 'Run Info',
+        data: [
+            { label: 'Environment', value: runInfo['env'] },
+            { label: 'Agency RFP URL', value: runInfo['agencyUrl'] },
+            { label: 'Stratasphere URL', value: runInfo['ssphereUrl'] },
+            { label: 'Email URL', value: runInfo['mailinatorUrl'] },
+            { label: 'Cypress Version', value: runInfo['cypressVersion'] },
+            { label: 'Node Version', value: runInfo['nodeVersion'] },
+            {
+                label: 'Execution Start Time',
+                value: dayjs(runInfo['startedTestsAt']).format('YYYY-MM-DD HH:mm:ss'),
+            },
+            {
+                label: 'Execution End Time',
+                value: dayjs(runInfo['endedTestsAt']).format('YYYY-MM-DD HH:mm:ss'),
+            },
+        ],
+    },
+    disableLog: true,
+    pageTitle: 'Cypress Cucumber Html Report',
+    displayDuration: true,
+});
